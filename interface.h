66fd6a6525c2bf8659a17572450ce2fff85a3e6a
Kmjal <vasakazakov75@gmail.com>
2026-06-12 13:21:24 +0500

    Trigger automated NDK build

--- interface.h
+++ interface.h
@@ -1,9 +1,13 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+typedef void* (*CreateInterfaceFn)(const char* pName, int* pReturnCode);
+
 class IBaseInterface {
 public:
     virtual ~IBaseInterface() {}
 };
 
-#endif
+#define EXPORT_FUNCTION extern "C" __attribute__((visibility("default")))
+
+#endif // INTERFACE_H
--- main.cpp
+++ main.cpp
@@ -15,40 +15,68 @@ public:
     virtual void OnMouseMove(int x, int y) = 0;
     virtual void OnMouseClick(int button, bool down) = 0;
     virtual void Update() = 0;
-    
-    virtual void Padding0() = 0;
-    virtual void Padding1() = 0;
-    virtual void Padding2() = 0;
-    virtual void Padding3() = 0;
-    virtual void Padding4() = 0;
+    virtual void OnKeyType(int key, bool down) = 0;
+    virtual void RunJavaScript(const char* script) = 0;
+    virtual void ClearHistory() = 0;
+    virtual void GoBack() = 0;
+    virtual void GoForward() = 0;
+    virtual bool IsLoading() = 0;
+    virtual const char* GetCurrentURL() = 0;
+    virtual void SetPaintTarget(void* pTarget) = 0;
 };
 
 class CGModWebBrowser : public IGModWebBrowser {
+private:
+    char m_szCurrentUrl[512];
 public:
-    CGModWebBrowser() {}
+    CGModWebBrowser() {
+        std::strncpy(m_szCurrentUrl, "about:blank", sizeof(m_szCurrentUrl));
+    }
     virtual ~CGModWebBrowser() {}
 
     void Init() override { 
-        if (pConMsg) pConMsg("[GModBrowser] Initialization routine executed.\n"); 
+        if (pConMsg) pConMsg("[GModBrowser] Init: Bridge connection established.\n"); 
     }
+    
     void Shutdown() override {
-        if (pConMsg) pConMsg("[GModBrowser] Shutdown routine executed.\n"); 
+        if (pConMsg) pConMsg("[GModBrowser] Shutdown: Cleaning resources.\n"); 
     }
+    
     void SetSize(int w, int h) override {
-        if (pConMsg) pConMsg("[GModBrowser] Size set: %dx%d\n", w, h);
+        if (pConMsg) pConMsg("[GModBrowser] SetSize: Viewport dimensions set to %dx%d\n", w, h);
     }
+    
     void LoadURL(const char* url) override {
-        if (pConMsg && url) pConMsg("[GModBrowser] URL requested: %s\n", url);
+        if (url) {
+            std::strncpy(m_szCurrentUrl, url, sizeof(m_szCurrentUrl));
+            if (pConMsg) pConMsg("[GModBrowser] LoadURL: Navigating to %s\n", m_szCurrentUrl);
+        }
     }
+    
     void OnMouseMove(int x, int y) override {}
     void OnMouseClick(int button, bool down) override {}
     void Update() override {}
+    void OnKeyType(int key, bool down) override {}
     
-    void Padding0() override {}
-    void Padding1() override {}
-    void Padding2() override {}
-    void Padding3() override {}
-    void Padding4() override {}
+    void RunJavaScript(const char* script) override {
+        if (pConMsg && script) pConMsg("[GModBrowser] JavaScript executed in context.\n");
+    }
+    
+    void ClearHistory() override {}
+    void GoBack() override {}
+    void GoForward() override {}
+    
+    bool IsLoading() override { 
+        return false; 
+    }
+    
+    const char* GetCurrentURL() override { 
+        return m_szCurrentUrl; 
+    }
+    
+    void SetPaintTarget(void* pTarget) override {
+        if (pConMsg && pTarget) pConMsg("[GModBrowser] Paint target context updated.\n");
+    }
 };
 
 EXPORT_FUNCTION void* CreateInterface(const char* pName, int* pReturnCode) {
@@ -62,8 +90,8 @@ EXPORT_FUNCTION void* CreateInterface(const char* pName, int* pReturnCode) {
         }
     }
 
-    if (pConMsg) {
-        pConMsg("[GModBrowser] CreateInterface requested: %s\n", pName);
+    if (pConMsg && pName) {
+        pConMsg("[GModBrowser] Factory query for interface: %s\n", pName);
     }
 
     if (pName && std::strstr(pName, "IGModWebBrowser")) {
